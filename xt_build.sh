9de04221ac4ec2695e0781f214a4acf224f6bbe6
Sander Spies <sanderspies@gmail.com>
2020-01-02 23:27:20 +0100

    Use newly added download script.

--- xt_build.sh
+++ xt_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/opt/GeoLite2xtables/00_download_geolite2
+/opt/GeoLite2xtables/download_geolite2.sh
 /opt/GeoLite2xtables/10_download_countryinfo
 cat /tmp/GeoLite2-Country-Blocks-IPv{4,6}.csv | /opt/GeoLite2xtables/20_convert_geolite2 /tmp/CountryInfo.txt > /xt_build/GeoIP-legacy.csv
 /usr/libexec/xtables-addons/xt_geoip_build -D /xt_build /xt_build/GeoIP-legacy.csv
